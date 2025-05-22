0f9a83253668a8df0677d0de078b63ae54e04fa0
dayayas2 <dayayas2@gmail.com>
2025-05-22 15:37:29 +0900

    [ADD] 0522

--- Assets/02.Scripts/StudyComponent.cs
+++ Assets/02.Scripts/StudyComponent.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class StudyComponent : MonoBehaviour
 {
-    public GameObject obj; // 큐브 게임 오브젝트
+    public GameObject obj;
 
-    public string changeName; // 유니티에서 설정할 문자열 변수
-    
     void Start()
     {
-        obj.name = changeName; // 큐브 오브젝트의 이름을 바꾸는 기능
+        // Player라는 Tag를 가진 게임오브젝트를 찾아서 obj에 할당
+        obj = GameObject.FindGameObjectWithTag("Player");
+
+        Debug.Log(obj.name); // 게임오브젝트의 이름
+        Debug.Log(obj.tag); // 게임오브젝트의 태그
+        Debug.Log(obj.transform.position); // 게임오브젝트의 Transform 컴포넌트의 위치
+        Debug.Log(obj.transform.rotation); // 게임오브젝트의 Transform 컴포넌트의 회전
+        Debug.Log(obj.transform.localScale); // 게임오브젝트의 Transform 컴포넌트의 크기
+
     }
 
- 
 }
